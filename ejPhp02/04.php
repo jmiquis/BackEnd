f03cff14482156bb8a86ab7cd916b5f1ab0d751d
Jorge <jmiquis87@gmail.com>
2021-09-24 18:18:25 +0200

    ejercicio 4

---
+++ ejPhp02/04.php
@@ -0,0 +1,34 @@
+<!-- Realizar un programa en php que genere 50 números aleatorios en 1 y 100 y que muestre en una tabla  html el valor máximo, el mínimo y la media con el siguiente formato:
+  Nota definir los valores 50 y 100 como constantes en PHP (define) -->
+
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <?php require_once('funcionesvar.php');?>
+      <style>
+          table,td,tr{
+              border: 1px solid black;
+              border-collapse: collapse;
+          }
+          th{
+              background-color: black;
+              color: white;
+          }
+      </style>
+  </head>
+  <body>
+        <table>
+            <th colspan="2">generacion 50 valores aleatorios</th>
+                <?php for ($i=0; $i <count($arrayNombres) ; $i++):?>
+                    <tr>
+                        <td><?= $arrayNombres[$i]?></td>
+                        <td><?= $arrayNumeros[$i]?></td>
+                    </tr>
+                <?php endfor ?>
+        </table>
+  </body>
+  </html>
--- ejPhp02/funcionesvar.php
+++ ejPhp02/funcionesvar.php
@@ -39,4 +39,24 @@
             echo("La potencia de $oper1 y $oper2 = $resul <br>");
         }
 
+
+        //ejercicio 4
+        define("TOTAL",50);
+        define("LIMITE",100);
+        $numero4=0;
+        $mayor=0;
+        $menor=101;
+        $suma=0;
+        $arrayNumeros;
+        $arrayNombres=[0=>"minimo",1=>"maximo",2=>"media"];
+
+        for ($i=0; $i < TOTAL; $i++) {
+            $numero4=random_int(1,LIMITE);
+            $suma+=$numero4;
+
+            $mayor=($numero4>$mayor)?$numero4:$mayor;
+            $menor=($numero4<$menor)?$numero4:$menor;
+        }
+            $media=$suma/TOTAL;
+            $arrayNumeros=[0=>$menor,1=>$mayor,2=>$media];
 ?>
