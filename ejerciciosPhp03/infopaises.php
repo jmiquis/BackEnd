ea7ce7fc8a7f0fc6099b3d5bb445e6936b5a69dc
Jorge <jmiquis87@gmail.com>
2021-10-01 17:30:17 +0200

    ej 6

--- ejerciciosPhp03/infopaises.php
+++ ejerciciosPhp03/infopaises.php
@@ -49,15 +49,3 @@ $ciudades = [
   }
 $arrayPaises2=version2($paises);
   ?>
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  </head>
-  <body>
-    <?=var_dump($arrayPaises2)?>
-  </body>
-  </html>
