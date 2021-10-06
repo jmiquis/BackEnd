9ef0f3f9920856b6fc69c3bfbae76a57edc40de7
Jorge <jmiquis87@gmail.com>
2021-10-06 16:11:26 +0200

    orden arreglado los numeros estan en orden el las columnas

--- cartonBingo/cartonBingo.php
+++ cartonBingo/cartonBingo.php
@@ -13,7 +13,7 @@ $posiciones=posicionesIni();
 //selecciona 3 columnas al azar para que solo tengan 1 numero
 $columnasCon1Elemento=array_rand($posiciones,3);
 
-//selecciona al azar la posicion de la columna donde va el numero que no es 0
+//selecciona al azar la posicion de la columna donde va el numero que no es 0 en las columnas con 1 numero
 function introduceCeros(&$posiciones,$columnasCon1Elemento){
     for ($i=0; $i <count($columnasCon1Elemento) ; $i++) {
         $posicion=$columnasCon1Elemento[$i];
@@ -26,7 +26,11 @@ introduceCeros($posiciones,$columnasCon1Elemento);
 function colocaNoCeros(&$posiciones){
     foreach ($posiciones as $key => $value) {
         if (is_array($value)) {
-            $aleatorios=array_rand([0,1,2],2);//para evitar valores repetidos
+            $aleatorios=array_rand([0,1,2],2);
+            do{//para evitar valores repetidos
+                $aleatorios=array_rand([0,1,2],2);
+            }while($aleatorios[0]==$aleatorios[1]);
+
              for ($i=0; $i <count($value) ; $i++) {
                  $posiciones[$key][$i]=$aleatorios[$i];
              }
@@ -44,8 +48,12 @@ function eliminaRepetidas(&$posiciones){
             if (is_array($posiciones[$i])) {
                 do{
                     for ($j=0; $j <count($posiciones[$i+1]) ; $j++) {
+                        $aleatorios=array_rand([0,1,2],2);
+                        do{
                         $aleatorios=array_rand([0,1,2],2);//para evitar valores repetidos
+                        }while($aleatorios[0]==$aleatorios[1]);
                         $posiciones[$i+1][$j]=$aleatorios[$j];
+
                     }
                 }while($posiciones[$i]===$posiciones[$i+1]);
             }
@@ -59,6 +67,9 @@ function eliminaRepetidas(&$posiciones){
 }
 eliminaRepetidas($posiciones);
 
+
+
+
 //inicia una matriz que representa un carton con todos los elementos a 0
  function iniCarton(){
     $carton=[];
@@ -69,21 +80,31 @@ eliminaRepetidas($posiciones);
 }
 $carton=iniCarton();
 
+//genera un array de valores aleatorios para rellenar el carton
 
+function generaAleatorios($columna){
+    $inicio=($columna==0)?1:(($columna+1)*10);
+    $final=($columna==0)?9:$inicio+9;
+    $aleatorio=[];
+    for ($i=$inicio,$indice=0; $i <=$final ; $i++,$indice++) {
+        $aleatorio[$i]=$indice;
+    }
+    return array_rand($aleatorio,2);
+}
 //recorre el array de posiciones pintando uno o dos numeros en el carton
 function rellenaCarton(&$carton,$posiciones){
 foreach ($posiciones as $key => $value) {
     $columna=$key;
     if (is_array($value)) {
+        $aleatorios=generaAleatorios($columna);
         for ($i=0; $i <count($value) ; $i++) {
             $fila=$value[$i];
-            $numero=($columna==0)?random_int(1,10):random_int(($columna*10),$columna*10+9);
-            $carton[$fila][$columna]=$numero;
+            $carton[$fila][$columna]=$aleatorios[$i];
         }
     }
     else{
         $fila=$value;
-        $numero=($columna==0)?  random_int(1,10):random_int(($columna*10),$columna*10+9);
+        $numero=($columna===0)?random_int(1,10):random_int(($columna*10),$columna*10+9);
         $carton[$fila][$columna]=$numero;
     }
 }
@@ -97,18 +118,31 @@ rellenaCarton($carton,$posiciones);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        table,td,tr{
+            size: 50px;
+            border: 1px black solid;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 <body>
     <table>
-        <?php foreach ($carton as $key => $value):?>
-            <tr>
-                <?php foreach($value as $clave=>$valor):?>
-                    <td>
-                        <?=$valor?>
-                    </td>
-                <?php endforeach?>
-            </tr>
-        <?php endforeach?>
+        <code>
+            <?php foreach ($carton as $key => $value):?>
+                <tr>
+                    <?php foreach($value as $clave=>$valor):?>
+                        <td>
+                            <?php if($valor!=0):?>
+                                <?= $valor?>
+                            <?php else:?>
+                                *
+                            <?php endif?>
+                        </td>
+                    <?php endforeach?>
+                </tr>
+            <?php endforeach?>
+        </code>
     </table>
 </body>
 </html>
