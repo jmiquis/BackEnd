7427875d1dc9082e7d0fc68aae8bcf8b512b3656
Jorge <jmiquis87@gmail.com>
2021-10-27 19:49:25 +0200

    perfil ok

--- ejerciciosPhp06/perfil.php
+++ ejerciciosPhp06/perfil.php
@@ -8,19 +8,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <?php
+        if(isset($_COOKIE["deportes"])){
+            $arrayDeportes=explode(",",$_COOKIE["deportes"]);
+        }
+    ?>
+
     <form action="perfil.php" method="get">
-        <p>edad: <input type="number" name="edad" id=""></p>
-        <p>genero:  <input type="radio" name="genero" value="masculino"> <input type="radio" name="genero" value="femenino"></p>
+        <p>edad: <input type="number" name="edad" id="" value="<?=(isset($_COOKIE["edad"]))?$_COOKIE["edad"]:''?>"></p>
+        <p>genero:
+        masculino <input type="radio" name="genero" value="masculino" <?=(isset($_COOKIE["genero"])&& $_COOKIE["genero"]=="masculino")?"checked":"";?>>
+        femenino <input type="radio" name="genero" value="femenino" <?=(isset($_COOKIE["genero"])&& $_COOKIE["genero"]=="femenino")?"checked":"";?>></p>
         <p>deportes:</p>
         <select name="deportes[]" id="" multiple>
-            <option value="futbol">futbol</option>
-            <option value="baloncesto">basket</option>
-            <option value="tenis">tenis</option>
-            <option value="ping pong">ping pong</option>
+            <option value="futbol"<?=(isset($arrayDeportes)&& in_array("futbol",$arrayDeportes))?"selected":""?>>futbol</option>
+            <option value="baloncesto" <?=(isset($arrayDeportes)&& in_array("baloncesto",$arrayDeportes))?"selected":""?>>basket</option>
+            <option value="tenis" <?=(isset($arrayDeportes)&& in_array("tenis",$arrayDeportes))?"selected":""?>>tenis</option>
+            <option value="ping pong"<?=(isset($arrayDeportes)&& in_array("ping pong",$arrayDeportes))?"selected":""?>>ping pong</option>
         </select>
         <p><input type="submit" name="orden" value="almacenar datos">
             <input type="submit" name="orden" value="borrar datos"></p>
     </form>
+    <?php if (isset($_GET["orden"])):?>
+
+    <?php
+
+
+        function crearCookies($tiempo){
+            if (isset($_REQUEST["edad"])) {
+                setcookie("edad",$_REQUEST["edad"],time()+$tiempo);
+            }
+            if (isset($_REQUEST["genero"])) {
+                setcookie("genero",$_REQUEST["genero"],time()+$tiempo);
+            }
+            if(isset($_REQUEST["deportes"])){
+                setcookie("deportes",implode(",",$_REQUEST["deportes"]),time()+$tiempo);
+            }
+        }
+
+        if ($_GET["orden"]=="borrar datos") {
+            crearCookies(-1);
+        }
+        elseif ($_GET["orden"]=="almacenar datos") {
+            crearCookies(300);
+        }
+        header("location:perfil.php");
+    ?>
+
+
+    <?php endif?>
 </head>
 <body>
 
