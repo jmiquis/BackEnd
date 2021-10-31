5ecd28be596626f5e0afa5861cddefd2582314ff
Jorge <jmiquis87@gmail.com>
2021-10-31 23:09:52 +0100

    visitas ok

--- cassino.php/app/entrada.php
+++ cassino.php/app/entrada.php
@@ -1,6 +1,6 @@
 
     <form action="index.php" method="GET">
-    <p>esta es su visita numero <?=$_COOKIE["visitas"]?></p>
+    <p>esta es su visita numero <?=$nvisitas?></p>
     <p>introduzca el dinero con el que quiere jugar hoy y pulse enter
         <input type="text" name="dinero">
     </p>
--- cassino.php/app/tirada.php
+++ cassino.php/app/tirada.php
@@ -1,5 +1,4 @@
 
-esta es su visita numero <?=$nvisitas+1?>
     su apuesta:
     <p>dinero apostado : <?=$_SESSION["apuesta"]?></p>
     <p>apuesta: <?=$_SESSION["parImpar"]?></p>
